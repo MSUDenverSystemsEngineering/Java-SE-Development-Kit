fd1db5fa305597ddf841e167349c6f8dc47b342b
truongnguyen-msudenver <tnguy189@msudenver.edu>
2019-07-18 18:11:41 -0600

    Update Deploy-Application.ps1
    Add Environment Path

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -155,7 +155,8 @@ Try {
 		[string]$installPhase = 'Post-Installation'
 
 		## <Perform Post-Installation tasks here>
-
+		## add the full path of the jdk-12\bin directory to the PATH variable
+		[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + "C:\Program Files\Java\jdk-12\bin", "Machine")
 		## Display a message at the end of the install
 		If (-not $useDefaultMsi) {
 
